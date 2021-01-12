2acc66c1d86867c9b4619d9edabaf78d76b2d03a
hoangttse63415 <41440734+hoangttse63415@users.noreply.github.com>
2021-01-12 13:36:32 +0700

    qrscanner

---
+++ lib/screen/checkin_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:barcode_widget/barcode_widget.dart';
+import 'package:user_library/widget/checkin/scanner.dart';
+
+class Checkin_Screen extends StatefulWidget {
+  Checkin_Screen({Key key}) : super(key: key);
+
+  final Map<String, dynamic> pluginParameters = {};
+
+  @override
+  _Checkin_ScreenState createState() => _Checkin_ScreenState();
+}
+
+class _Checkin_ScreenState extends State<Checkin_Screen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.only(top: 100.0),
+          child: Column(
+            children: [
+              BarcodeWidget(
+                barcode: Barcode.qrCode(),
+                color: Colors.black87,
+                data: 'dm dat',
+                width: 200,
+                height: 200,
+              ),
+              SizedBox(height: 20.0),
+              RaisedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => Scanner_Screen(),
+                    ),
+                  );
+                },
+                child: Text('Scan QR'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
